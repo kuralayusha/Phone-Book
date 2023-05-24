c9f5bc4e614a9a4bb44bde63c742f9c18e77540d
kurlayusha <tbark.guard@gmail.com>
2023-05-24 16:36:00 +0300

    Add Person Info tamplate done

--- src/app/show-components.service.ts
+++ src/app/show-components.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ShowComponentsService {
-  showAddPersonComponent: boolean = false;
+  showAddPersonComponent: boolean = true;
   showEditPersonComponent: boolean = false;
   showSearchbarComponent: boolean = false;
   showPersonListComponent: boolean = false;

e15a79c1416a537a992c7b59fc08bab305ad139f
kurlayusha <tbark.guard@gmail.com>
2023-05-26 20:11:16 +0300

    added comments to data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -9,23 +9,27 @@ export class DataService {
   private storageKey = 'phoneBookData'; // Adjust the storage key according to your application
   private dataUpdateSubject = new Subject<void>();
   
+  // sets the data to the local storage
   setDatas(data: any[]) {
     const newData = [...data];
     localStorage.setItem(this.storageKey, JSON.stringify(newData));
     this.dataUpdateSubject.next();
   }
 
+  // returns the data from the local storage
   getDatas(): any[] {
     const data = localStorage.getItem(this.storageKey);
     return data ? JSON.parse(data) : [];
   }
 
+  // adds the new contact to the data
   addPersonToData(person: any) {
     const data = this.getDatas();
     const newData = [...data, person];
     this.setDatas(newData);
   }
 
+  // updates the selected contact from the data
   updatePersonFromData(updatedPerson: any) {
     const data = this.getDatas();
     const updatedData = data.map((person) => {
@@ -37,15 +41,15 @@ export class DataService {
     this.setDatas(updatedData);
   }
 
+  // deletes the selected contact from the data
   deletePersonFromData(personId: any) {
     const data = this.getDatas();
     const updatedData = data.filter((person) => person.id !== personId);
     this.setDatas(updatedData);
   }
 
+  // when a new contact is added, this function is called to create a new id
   createId() {
-    // look to the data and find the highest id and add 1 to it
-    // if there is no data, return 1
     const data = this.getDatas();
     if (data.length === 0) {
       return 1;
@@ -55,6 +59,7 @@ export class DataService {
     return maxId + 1;
   }
 
+  // this observable is used to notify the components when the data is updated
   getDataUpdateObservable(): Observable<void> {
     return this.dataUpdateSubject.asObservable();
   }

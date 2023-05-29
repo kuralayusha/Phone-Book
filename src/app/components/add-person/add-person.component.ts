ff97eb964e8d9f946f8d98d1c3eba62b044a3888
unknown <tbark.guard@gmail.com>
2023-05-29 12:45:49 +0300

    dummy data + date edit

--- src/app/components/add-person/add-person.component.ts
+++ src/app/components/add-person/add-person.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter,OnInit  } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 
 import { DataService } from 'src/app/services/data.service';
 
@@ -6,14 +6,12 @@ import { PersonInfo } from 'src/app/interfaces/PersonInfo';
 
 import { PrimeNGConfig } from 'primeng/api';
 
-
 @Component({
   selector: 'app-add-person',
   templateUrl: './add-person.component.html',
   styleUrls: ['./add-person.component.css'],
 })
-export class AddPersonComponent  {
-  
+export class AddPersonComponent {
   createdPerson: PersonInfo = {
     id: null,
     firstName: '',
@@ -26,16 +24,26 @@ export class AddPersonComponent  {
 
   @Output() messageEvent = new EventEmitter<string>();
 
+  // handleBirthday(birthday: any) {
+  //   // take the birthday string and delete from the start of "t" character to the end of the string
+  //   let birthdayString = birthday.slice(1, birthday.length);
+
+  //   return (this.createdPerson.birthday = birthdayString);
+  // }
+
   onAdd() {
     if (this.createdPerson.firstName && this.createdPerson.phone) {
+      // if (this.createdPerson.birthday) {
+      //   this.handleBirthday(this.createdPerson.birthday);
+      // }
       this.createdPerson.id = this.dataService.createId();
       this.dataService.addPersonToData(this.createdPerson);
-      this.messageEvent.emit(this.setAddToFalse);      
+      this.messageEvent.emit(this.setAddToFalse);
     } else {
       alert('Please enter a name and a phone number!');
     }
   }
-  
+
   onCancel() {
     this.messageEvent.emit(this.setAddToFalse);
   }
